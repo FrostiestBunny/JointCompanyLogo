b804baec4c0f26a4858a82f8e6efc20429e7b08f
FrostiestBunny <acezackfair@gmail.com>
2024-01-20 21:34:54 +0100

    Add project files.

---
+++ Patches/LogoPatch.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace JointCompanyLogo.Patches
+{
+    [HarmonyPatch(typeof(MenuManager), "Awake")]
+    public static class LogoPatch
+    {
+        [HarmonyPriority(Priority.Last)]
+        public static void Postfix(MenuManager __instance)
+        {
+            try
+            {
+                // Main Menu
+                GameObject parent = __instance.transform.parent.gameObject;
+                GameObject menuContainer = parent.transform.Find("MenuContainer").gameObject;
+                if (menuContainer.scene.name != "MainMenu") return;
+                GameObject logo = menuContainer.transform.Find("MainButtons").Find("HeaderImage").gameObject;
+
+                Image image = logo.GetComponent<Image>();
+                image.sprite = Sprite.Create(Plugin.mainLogo, new Rect(0, 0, Plugin.mainLogo.width, Plugin.mainLogo.height), new Vector2(0.5f, 0.5f));
+
+                // Loading screen
+                GameObject loadingScreenImage = menuContainer.transform.Find("LoadingScreen").Find("Image").gameObject;
+                Image loadingImage = loadingScreenImage.GetComponent<Image>();
+                loadingImage.sprite = Sprite.Create(Plugin.mainLogo, new Rect(0, 0, Plugin.mainLogo.width, Plugin.mainLogo.height), new Vector2(0.5f, 0.5f));
+            }
+            catch (Exception e)
+            {
+                Plugin.StaticLogger.LogError(e.Message);
+            }
+        }
+
+        private static void Traverse(GameObject obj)
+        {
+            Plugin.StaticLogger.LogInfo(obj.name);
+            foreach (Transform child in obj.transform)
+            {
+                Traverse(child.gameObject);
+            }
+            Plugin.StaticLogger.LogInfo("-----------");
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using JointCompanyLogo.Utils;
+using UnityEngine;
+using System.Reflection;
+
+namespace JointCompanyLogo
+{
+    public static class PluginInformation
+    {
+        public const string PLUGIN_NAME = "JointCompanyLogo";
+        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_GUID = "me.frost.bunny.jointcompanylogo";
+    }
+
+    [BepInPlugin(PluginInformation.PLUGIN_GUID, PluginInformation.PLUGIN_NAME, PluginInformation.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Texture2D mainLogo;
+        public static ManualLogSource StaticLogger;
+
+        private Harmony harmony;
+
+        private void Awake()
+        {
+            StaticLogger = Logger;
+
+            // Plugin startup logic
+            harmony = new Harmony(PluginInformation.PLUGIN_GUID);
+            harmony.PatchAll();
+
+            AssetBundle bundle = BundleUtilities.LoadBundleFromInternalAssembly("jointcompany.assets", Assembly.GetExecutingAssembly());
+            foreach (var name in bundle.GetAllAssetNames())
+            {
+                Logger.LogInfo($"Loading asset: {name}");
+            }
+
+            mainLogo = bundle.LoadPersistentAsset<Texture2D>("assets/jointlogo.png");
+            bundle.Unload(false);
+
+            Logger.LogInfo($"Resolution {mainLogo.width} x {mainLogo.height}");
+
+            Logger.LogInfo($"Plugin {PluginInformation.PLUGIN_NAME} is loaded!");
+        }
+
+    }
+}
